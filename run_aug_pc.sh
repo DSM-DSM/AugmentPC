22d51d26aa7b27c407bd8b384d334a87accd7b4a
jinyue-yu <1932428206@qq.com>
2026-03-01 09:54:37 +0800

    modify data augmentation code and figures

--- run_aug_pc.sh
+++ run_aug_pc.sh
@@ -2,13 +2,13 @@
 
 #SBATCH --account=u2024104095
 #SBATCH --comment=causal_learning
-#SBATCH --job-name=10_3_pc_kci_gt_graph_nn_100
+#SBATCH --job-name=50_2_fdr_pc_polynomial_Hyb_0.2
 #SBATCH --nodes=1
 
-#SBATCH --partition=cpu64c # cpu64c6530, cpu64c1t, cpu64c, cpu40c, cpu24c, gpu-titan, gpu-5090
+#SBATCH --partition=cpu64c1t # cpu64c6530, cpu64c1t, cpu64c, cpu40c, cpu24c, gpu-titan, gpu-5090
 ###SBATCH --gres=gpu:1
 #SBATCH --ntasks=64
-#SBATCH --output=logs/hpc/10_3_pc_kci_gt_graph_nn_100.out
+#SBATCH --output=logs/hpc/50_2_fdr_pc_polynomial_Hyb_0.2.out
 
 ### hostname
 # 使用 bash 显式解释器
@@ -18,4 +18,4 @@ eval "$(/opt/app/anaconda3/bin/conda shell.bash hook)"
 ### export CUDA_VISIBLE_DEVICES=0
 source activate causal_learning
 
-python run_aug_pc.py --config augment_pc_config_10.yml --seed 8888
+python run_aug_pc.py --config augment_pc_config_50.yml --seed 8888
